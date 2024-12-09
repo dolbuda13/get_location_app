46d64bd48597b3cc9946576ae3e46c50b209d955
dolbuda13 <dolbuda24@gmail.com>
2024-12-09 20:25:23 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'views/home_page.dart';
+
+void main() {
+  runApp(ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '위치 기반 검색 앱',
+      home: HomePage(),
+    );
+  }
+}
