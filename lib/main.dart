26c73e69ed1615ef48474a79fb6d8383f1bbc53b
dolbuda13 <dolbuda24@gmail.com>
2024-12-09 20:28:02 +0900

    feat: 의존성 추가, main 구현, 파일 생성

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'views/home_page.dart';
 
 void main() {
-  runApp(ProviderScope(child: MyApp()));
+  runApp(ProviderScope(child: MyApp())); //providerscope을 사용하여 riverpod이 viewmodel 관리 가능
 }
 
 class MyApp extends StatelessWidget {
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: '위치 기반 검색 앱',
-      home: HomePage(),
+      //home: HomePage(),
     );
   }
-}
+}  
