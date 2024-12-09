85793f4bb3965262f2754f8b0d65a1e3dbdc7716
dolbuda13 <dolbuda24@gmail.com>
2024-12-09 21:25:11 +0900

    feat: location 레포지토리 구현

---
+++ lib/repositories/location_repository.dart
@@ -0,0 +1,32 @@
+import 'package:dio/dio.dart';
+import '../models/location.dart';
+
+class LocationRepository {
+  final Dio _dio = Dio();
+  final String _baseUrl = "https://openapi.naver.com/v1/search/local.json";
+  final String _clientId = "";
+  final String _clientSecret = "";//개인정보 올리지 않도록 주의
+
+  Future<List<Location>> searchLocations(String query) async {
+    //네트워크 통신은 반드시 try catch 필요
+    try {
+      final response = await _dio.get(
+        _baseUrl,
+        queryParameters: {"query": query, "display":5},
+        options: Options(headers: {
+          "X-Naver-Client-Id": _clientId,
+          "X-Naver-Client-Secret": _clientSecret,
+        }),
+      );
+    if (response.statusCode == 200) {//get 요청 성공일 때
+        final List data = response.data['items'];
+        return data.map((item) => Location.fromJson(item)).toList();
+      }
+        return [];
+      
+    } catch (e) {//에러 났을 때
+      print(e);
+      return [];
+    }
+  }
+}
