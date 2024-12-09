2979f95d0b68267646c0707231211b03295556f4
dolbuda13 <161493910+dolbuda13@users.noreply.github.com>
2024-12-09 22:03:48 +0900

    Update location_repository.dart

--- lib/repositories/location_repository.dart
+++ lib/repositories/location_repository.dart
@@ -4,8 +4,8 @@ import '../models/location.dart';
 class LocationRepository {
   final Dio _dio = Dio();
   final String _baseUrl = "https://openapi.naver.com/v1/search/local.json";
-  final String _clientId = "j8QM40UBOnvHcWEkvgZ3";
-  final String _clientSecret = "dEplC8eHLF";
+  final String _clientId = "";
+  final String _clientSecret = "";
 
   Future<List<Location>> searchLocations(String query) async {
     try {
